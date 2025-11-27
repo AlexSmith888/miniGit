56de1d16ea96d3d34391368080e6016a30ec7a11
Alexsmith888 <alexkoshechkin888@gmail.com>
2025-11-27 21:10:50 +0300

    track deletes from temp when target not exists

--- src/main/java/test.java
+++
@@ -1,6 +0,0 @@
-/*public class test {
-    static void main() throws IOException {
-        new CommitCommand().execute(new String[]{"commit", "/home/alex/test/a"});
-        Files.createDirectory(Path.of("/home/test/alex/a/miniGit/commits/651fc5e"));
-    }
-}*/
